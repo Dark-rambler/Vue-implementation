049649bf4bb3129b503249a131bb237859d4e578
edwin garcia <garciaramiro000@gmail.com>
2025-02-01 07:46:32 -0400

    inicializa el proyecto solo con cdn

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+    </ul>
+  </nav>
+
+  <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+export default {
+  name: 'Navbar',
+};
+</script>
