e78e8efdbdfc191b008b26224155fc68449ee80d
edwin garcia <garciaramiro000@gmail.com>
2025-02-01 08:54:52 -0400

    agrega la tabla, aun falta hacer el consumo de api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,36 @@
 
 <template>
   <main>
-    <h1>This is the home page</h1>
+    <div class=" mt-5 ">
+      <table class="table table-hover border">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="cursor-pointer">
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr class="cursor-pointer">
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
