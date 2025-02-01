a0b87877821094d4e077b9c277b030881b3664f4
edwin garcia <garciaramiro000@gmail.com>
2025-02-01 08:21:37 -0400

    agrega header y footer a las rutas e implementa bootstrap

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,15 +1,22 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <RouterLink to="/">Home</RouterLink>
-      </li>
-    </ul>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink to="/" class="nav-link active" aria-current="page" href="#">Home</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
+</template>
 
-  <script setup lang="ts">
+<script setup lang="ts">
 import { RouterLink } from 'vue-router';
-export default {
-  name: 'Navbar',
-};
 </script>
